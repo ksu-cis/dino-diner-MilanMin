c9273228383738f53495f275dc68b86a06dbb0e4
milan <milan@CIS-RDS1>
2019-09-09 15:21:45 -0500

    On VelociWrap. Above completed. Using milestone as guide, working top to bottom.

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class DinoNuggets
+    {
+
+        private uint numNuggets = 6;
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { numNuggets+" Chicken Nuggets" };
+                return ingredients;
+            }
+        }
+
+        public DinoNuggets()
+        {
+            this.Price = 4.25;
+            this.Calories = 59*numNuggets;
+        }
+
+        public void AddNugget()
+        {
+            this.numNuggets++;
+        }
+    }
+}
---
+++ Menu/Entrees/VelociWrap.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class VelociWrap
+    {
+        private bool dressing = true;
+        private bool lettuce = true;
+        private bool cheese = true;
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Chicken Breast" };
+                ingredients.Add("Flour Tortilla");
+                if (dressing) ingredients.Add("Dressing");
+                if (lettuce) ingredients.Add("Lettuce");
+                return ingredients;
+            }
+        }
+
+        public VelociWrap()
+        {
+            this.Price = 6.86;
+            this.Calories = 356;
+        }
+
+        public void HoldDressing()
+        {
+            this.dressing = false;
+        }
+
+        public void HoldLettuce()
+        {
+            this.lettuce = false;
+        }
+    }
+}
